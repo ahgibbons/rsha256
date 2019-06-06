6a4630a94d163b5a9798b8f9b1b7061d7d821b27
Andrew Gibbons <andrew.h.gibbons@protonmail.com>
2019-06-07 08:52:06 +0900

    Finished sha256. Working version.

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,61 @@
-use std::fs;
 use std::io::Read;
-use std::io;
 use std::fs::File;
 use std::num::Wrapping;
+use std::env;
 
 mod shafuncs;
 
-static CHUNKSIZE_BYTES: usize = 64;
+macro_rules! wraparray {
+	( $( $x:expr),* ) => {
+		[$(Wrapping($x)),*]
+	}
+}
 
 fn main() {
-	let filename = "abc.txt";
-
+	let args: Vec<String> = env::args().collect();
+	let filename = &args[1];
 	let mut f = File::open(filename).expect("Could not open file.");
 
 	let mut buffer: [u8; 64] = [0; 64];
-
-	let mut digest: [Wrapping<u32>; 8]= [Wrapping(0x6a09e667),
-	Wrapping(0xbb67ae85),
-	Wrapping(0x3c6ef372),
-	Wrapping(0xa54ff53a),
-	Wrapping(0x510e527f),
-	Wrapping(0x9b05688c),
-	Wrapping(0x1f83d9ab),
-	Wrapping(0x5be0cd19)];
-
-	// declare variables
-
-
-	let mut readbyte;
+	let mut readlen;
 	let mut messagelength : u64 = 0;
-	while {readbyte = f.read(&mut buffer).expect("Could not read from file!"); readbyte==CHUNKSIZE_BYTES} {
-		messagelength += readbyte as u64;
-		hashround(digest, buffer)
-	}
-	messagelength += readbyte as u64;
-	shafuncs::padmessage(&mut buffer, readbyte, messagelength);
-	hashround(digest, buffer);
 
-	for i in K.iter() {
-		println!("{:?}", i);
-	}
+	let mut digest: [Wrapping<u32>; 8]= wraparray![
+			0x6a09e667,
+			0xbb67ae85,
+			0x3c6ef372,
+			0xa54ff53a,
+			0x510e527f,
+			0x9b05688c,
+			0x1f83d9ab,
+			0x5be0cd19];
 
-	println!("");
-	for i in K2.iter() {
-		println!("{:?}", i);
-	}
 
+	while {readlen = f.read(&mut buffer).expect("Could not read from file!"); readlen==shafuncs::CHUNKBYTES} {
+		messagelength += readlen as u64;
+		hashround(&mut digest, buffer)
+	}
+	messagelength += readlen as u64;
+	shafuncs::padmessage(&mut buffer, readlen, messagelength);
+	hashround(&mut digest, buffer);
+
+	// Final output
+	for i in digest.iter() {
+		print!("{:08x}", i);
+	}
+	println!("  {}", filename);
 }
 
 
 
-fn hashround(digest: [Wrapping<u32>; 8], bytebuffer: [u8; 64]) {
-	let mut hvars: [Wrapping<u32>; 8] = digest.clone();	
+fn hashround(digest: &mut [Wrapping<u32>; 8], bytebuffer: [u8; 64]) {
 	let mut t1: Wrapping<u32>;
 	let mut t2: Wrapping<u32>;
 
 	let wordbuffer = bytestowords(bytebuffer);
+
+	// Message Schedule
+	let w = shafuncs::message_schedule(wordbuffer);
 
 	let mut a: Wrapping<u32> = digest[0];
 	let mut b: Wrapping<u32> = digest[1];
@@ -66,11 +66,11 @@ fn hashround(digest: [Wrapping<u32>; 8], bytebuffer: [u8; 64]) {
 	let mut g: Wrapping<u32> = digest[6];
 	let mut h: Wrapping<u32> = digest[7];
 
-	let w = shafuncs::message_schedule(wordbuffer);
+
 
 	for t in 0..64 {
-		t1 = h + shafuncs::S1(e) + shafuncs::ch(e,f,g) + K[t] + w[t];
-		t2 = shafuncs::S0(a) + shafuncs::maj(a,b,c);
+		t1 = h + shafuncs::ls1(e) + shafuncs::ch(e,f,g) + K[t] + w[t];
+		t2 = shafuncs::ls0(a) + shafuncs::maj(a,b,c);
 		h = g;
 		g = f;
 		f = e;
@@ -80,6 +80,16 @@ fn hashround(digest: [Wrapping<u32>; 8], bytebuffer: [u8; 64]) {
 		b = a;
 		a = t1+t2;
 	}
+
+
+	digest[0] += a;
+	digest[1] += b;
+	digest[2] += c;
+	digest[3] += d;
+	digest[4] += e;
+	digest[5] += f;
+	digest[6] += g;
+	digest[7] += h;
 
 }
 
@@ -99,13 +109,8 @@ fn bytestowords(bytebuffer: [u8; 64]) -> [Wrapping<u32>; 16] {
 
 }
 
-macro_rules! wraparray {
-	( $( $x:expr),* ) => {
-		[$(Wrapping($x)),*]
-	}
-}
 
-const K2: [Wrapping<u32>; 64] = wraparray![
+const K: [Wrapping<u32>; 64] = wraparray![
 
 	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
   	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
@@ -116,14 +121,3 @@ const K2: [Wrapping<u32>; 64] = wraparray![
 	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
 	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
 	];
-
-const K: [Wrapping<u32>; 64] = [
-	Wrapping(0x428a2f98), Wrapping(0x71374491), Wrapping(0xb5c0fbcf), Wrapping(0xe9b5dba5), Wrapping(0x3956c25b), Wrapping(0x59f111f1), Wrapping(0x923f82a4), Wrapping(0xab1c5ed5),
-  	Wrapping(0xd807aa98), Wrapping(0x12835b01), Wrapping(0x243185be), Wrapping(0x550c7dc3), Wrapping(0x72be5d74), Wrapping(0x80deb1fe), Wrapping(0x9bdc06a7), Wrapping(0xc19bf174),
-	Wrapping(0xe49b69c1), Wrapping(0xefbe4786), Wrapping(0x0fc19dc6), Wrapping(0x240ca1cc), Wrapping(0x2de92c6f), Wrapping(0x4a7484aa), Wrapping(0x5cb0a9dc), Wrapping(0x76f988da),
-	Wrapping(0x983e5152), Wrapping(0xa831c66d), Wrapping(0xb00327c8), Wrapping(0xbf597fc7), Wrapping(0xc6e00bf3), Wrapping(0xd5a79147), Wrapping(0x06ca6351), Wrapping(0x14292967),
-	Wrapping(0x27b70a85), Wrapping(0x2e1b2138), Wrapping(0x4d2c6dfc), Wrapping(0x53380d13), Wrapping(0x650a7354), Wrapping(0x766a0abb), Wrapping(0x81c2c92e), Wrapping(0x92722c85),
-	Wrapping(0xa2bfe8a1), Wrapping(0xa81a664b), Wrapping(0xc24b8b70), Wrapping(0xc76c51a3), Wrapping(0xd192e819), Wrapping(0xd6990624), Wrapping(0xf40e3585), Wrapping(0x106aa070),
-	Wrapping(0x19a4c116), Wrapping(0x1e376c08), Wrapping(0x2748774c), Wrapping(0x34b0bcb5), Wrapping(0x391c0cb3), Wrapping(0x4ed8aa4a), Wrapping(0x5b9cca4f), Wrapping(0x682e6ff3),
-	Wrapping(0x748f82ee), Wrapping(0x78a5636f), Wrapping(0x84c87814), Wrapping(0x8cc70208), Wrapping(0x90befffa), Wrapping(0xa4506ceb), Wrapping(0xbef9a3f7), Wrapping(0xc67178f2)
-];
--- src/shafuncs.rs
+++ src/shafuncs.rs
@@ -1,10 +1,9 @@
 
 use std::num::Wrapping;
 const WORDSIZE_BITS: usize = 32;
-const ZEROPAD: u32 = 488;
-const CHUNKBITS: u32 = 512;
-const MLEN_BYTES: u32 = 8;
-
+const LENBYTES: usize = 8;
+pub const CHUNKBYTES: usize = 64;
+const ONEPAD: u8 = 0x80;
 
 fn rotr(x: Wrapping<u32>, n: usize) -> Wrapping<u32> {
 	(x >> n) | (x << (WORDSIZE_BITS - n) )
@@ -18,12 +17,12 @@ pub fn maj(x: Wrapping<u32>, y: Wrapping<u32>, z: Wrapping<u32>) -> Wrapping<u32
 	(x & y) ^ (x & z) ^ (y & z)
 }
 
-pub fn S0(x: Wrapping<u32>) -> Wrapping<u32> {
+pub fn ls0(x: Wrapping<u32>) -> Wrapping<u32> {
 	rotr(x, 2) ^ rotr(x,13) ^ rotr(x,22)
 }
 
-pub fn S1(x: Wrapping<u32>) -> Wrapping<u32> {
-	rotr(x,6) ^ rotr(x,11) ^ rotr(x,22)
+pub fn ls1(x: Wrapping<u32>) -> Wrapping<u32> {
+	rotr(x,6) ^ rotr(x,11) ^ rotr(x,25)
 }
 
 fn s0(x: Wrapping<u32>) -> Wrapping<u32> {
@@ -37,20 +36,16 @@ fn s1(x: Wrapping<u32>) -> Wrapping<u32> {
 pub fn padmessage(bytebuffer : &mut [u8], readbyte : usize, messagelength: u64) -> () {
 	let bitsize: u64 = messagelength * 8;
 	let mut pos: usize = readbyte ;
-	bytebuffer[pos] = 0x80;
-	let mut numzeros: u8 = 0;
+	bytebuffer[pos] = ONEPAD;
 	pos+=1;
-	for i in pos..(64-8) {
+	for i in pos..(CHUNKBYTES-LENBYTES) {
 		bytebuffer[i] = 0;
-		numzeros+=1;
 	}
-	
-	println!("{:064b}", bitsize);
+
 	let mut v: u8;
 	for i in 0..8 {
 		v = ((bitsize >> i*8) & 0xff) as u8;
 		bytebuffer[63-i] = v;
-		println!("{:08b}", v);
 	}
 
 }
@@ -58,9 +53,8 @@ pub fn padmessage(bytebuffer : &mut [u8], readbyte : usize, messagelength: u64) 
 pub fn message_schedule(wordbuffer: [Wrapping<u32>; 16]) -> [Wrapping<u32>; 64] {
 	let mut ms: [Wrapping<u32>; 64] = [Wrapping(0); 64];
 	ms[..16].clone_from_slice(&wordbuffer);
-	println!("{:?}", wordbuffer);
-	for i in ms.iter() {
-		println!("{}", i);
+	for t in 16..64 {
+		ms[t] = s1(ms[t-2]) + ms[t-7] + s0(ms[t-15]) + ms[t-16];
 	}
 
 	ms
